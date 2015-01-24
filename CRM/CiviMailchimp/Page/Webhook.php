90c3255cd05a096d2cd6333fa976c3e602389481
Anthony Nemirovsky <anemirovsky@giantrabbit.com>
2015-01-24 10:30:42 -0800

    Initial commit for Mailchimp webhook processing.

--- CRM/CiviMailchimp/Page/Webhook.php
+++ CRM/CiviMailchimp/Page/Webhook.php
@@ -6,6 +6,44 @@ class CRM_CiviMailchimp_Page_Webhook extends CRM_Core_Page {
   function run() {
     if (CRM_Utils_System::authenticateKey()) {
       parent::run();
+
+      $request_type = CRM_Utils_Request::retrieve('type', 'String');
+      $request_data = CRM_Utils_Request::retrieve('data', 'String');
+
+      $function_name = 'self::mailchimpWebhook' . ucwords($request_type);
+      if (is_callable($function_name)) {
+        call_user_func($function_name, $request_data);
+      }
     }
+  }
+
+  /**
+   * Add a Mailchimp subscriber to a CiviCRM Group.
+   */
+  static function mailchimpWebhookSubscribe($request_data) {
+  }
+
+  /**
+   * Remove a Mailchimp subscriber from a CiviCRM Group.
+   */
+  static function mailchimpWebhookUnsubscribe($request_data) {
+  }
+
+  /**
+   * Update a Mailchimp subscriber's email in CiviCRM.
+   */
+  static function mailchimpWebhookUpemail($request_data) {
+  }
+
+  /**
+   * Update a Mailchimp subscriber's Contact data in CiviCRM.
+   */
+  static function mailchimpWebhookProfile($request_data) {
+  }
+
+  /**
+   * Put a Mailchimp subscriber's email On Hold in CiviCRM.
+   */
+  static function mailchimpWebhookCleaned($request_data) {
   }
 }

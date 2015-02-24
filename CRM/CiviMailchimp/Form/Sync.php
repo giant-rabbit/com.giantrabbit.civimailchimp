e6558faddda5c82f7bc6e863eedc15fe921ff47b
Anthony Nemirovsky <anemirovsky@giantrabbit.com>
2015-02-24 12:45:49 -0800

    Bugfix for force sync failing.
    There was an extra legacy parameter that was being passed into one of the
    functions handling formating merge variables before sending an API request to
    Mailchimp. I've removed that now.

--- CRM/CiviMailchimp/Form/Sync.php
+++ CRM/CiviMailchimp/Form/Sync.php
@@ -41,7 +41,7 @@ class CRM_CiviMailchimp_Form_Sync extends CRM_Core_Form {
       $contact = CRM_CiviMailchimp_Utils::getContactById($contact['contact_id']);
       $email = CRM_CiviMailchimp_Utils::determineMailchimpEmailForContact($contact);
       $merge_fields = CRM_CiviMailchimp_Utils::getMailchimpMergeFields($mailchimp_sync_setting->mailchimp_list_id);
-      $merge_vars = CRM_CiviMailchimp_Utils::formatMailchimpMergeVars($merge_fields, $contact, $mailchimp_sync_setting);
+      $merge_vars = CRM_CiviMailchimp_Utils::formatMailchimpMergeVars($merge_fields, $contact);
       CRM_CiviMailchimp_Utils::subscribeContactToMailchimpList($mailchimp_sync_setting->mailchimp_list_id, $email, $merge_vars);
     }
 
